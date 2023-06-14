779a923578e2596ae0457086996a02a276234382
Forrest <1168948+ForrestRolf@users.noreply.github.com>
2023-06-14 14:17:35 +0800

    New page template: Configuration

---
+++ src/components/Configs.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <a-descriptions class="configs" bordered size="small" :column="1">
+    <a-descriptions-item label="Product">Cloud Database</a-descriptions-item>
+    <a-descriptions-item label="Billing">Prepaid</a-descriptions-item>
+    <a-descriptions-item label="Time">18:00:00</a-descriptions-item>
+    <a-descriptions-item label="Amount">$80.00</a-descriptions-item>
+    <a-descriptions-item label="Discount">$20.00</a-descriptions-item>
+    <a-descriptions-item label="Official">$60.00</a-descriptions-item>
+    <a-descriptions-item label="Config Info">
+      Data disk type: MongoDB
+      <br/>
+      Database version: 3.4
+      <br/>
+      Package: dds.mongo.mid
+      <br/>
+      Storage space: 10 GB
+      <br/>
+      Replication factor: 3
+      <br/>
+      Region: East China 1
+      <br/>
+    </a-descriptions-item>
+  </a-descriptions>
+</template>
+
+<style scoped>
+
+</style>
--- src/screen/Configuration.vue
+++ src/screen/Configuration.vue
@@ -1,11 +1,41 @@
 <script setup>
+import {ref} from "vue";
+import {FileTextOutlined, ControlOutlined} from "@ant-design/icons-vue";
+import Configs from "../components/Configs.vue";
 
+const activeKey = ref("system");
 </script>
 
 <template>
-  <a-empty />
+  <a-row :gutter="[10, 0]" class="config-container">
+    <a-col :span="24">
+      <a-tabs v-model:activeKey="activeKey" type="card" class="tabs min-h">
+        <a-tab-pane key="system">
+          <template #tab>
+            <span>
+              <ControlOutlined/>
+              System Configs
+            </span>
+          </template>
+          <Configs/>
+        </a-tab-pane>
+        <a-tab-pane key="dnodes">
+          <template #tab>
+            <span>
+              <FileTextOutlined/>
+              DNode Configs: 201-3
+            </span>
+          </template>
+          <Configs/>
+        </a-tab-pane>
+
+      </a-tabs>
+    </a-col>
+  </a-row>
 </template>
 
 <style scoped>
-
+.ant-typography {
+  margin-bottom: 0 !important;
+}
 </style>
